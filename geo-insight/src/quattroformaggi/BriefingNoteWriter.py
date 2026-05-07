3c7d7ce4a3f1e6eab34cdaf271499ae7d95c1d9b
pitygiusz <piotrek@jurczyk.biz>
2026-05-07 13:41:57 +0200

    fix output

--- geo-insight/src/quattroformaggi/BriefingNoteWriter.py
+++ geo-insight/src/quattroformaggi/BriefingNoteWriter.py
@@ -8,14 +8,16 @@ SYSTEM_PROMPT_PATH = PROJECT_ROOT / "prompts" / "BriefingNoteWriter.md"
 async def brief_writer(
     data_as_csv: str, message: str, articles: str, interpretation_notes: str | None = None
 ) -> str:
-    notes_instruction = (
-        f"\n5. **Interpretation Notes:** Add the following as the first paragraph of the briefing note:\n{interpretation_notes}\n"
+    
+    notes_instruction = f"This is the original user query: {message}."
+
+    notes_instruction += (
+        f"\nThis is summarization of the user query. Interpretate it at the beggining of the answer:\n{interpretation_notes}\n"
         if interpretation_notes
         else ""
     )
 
-    notes_instruction += f"This is the original user query: {message}."
-    notes_instruction += f"These are the most recent articles from that area and sector: {articles}. Summarise them in the introduction."
+    notes_instruction += f"\nThese are the most recent articles from that area and sector: {articles}. Summarise them in the introduction."
     
     system_prompt = SYSTEM_PROMPT_PATH.read_text().replace(
         "{{interpretation_notes}}", notes_instruction
